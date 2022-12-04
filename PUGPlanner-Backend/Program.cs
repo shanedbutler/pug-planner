5d94ddd4e414f06b409876ede9db9e906a208b27
shanedbutler <shanedbutler@gmail.com>
2022-12-04 16:29:02 -0500

    Initialize project

---
+++ PUGPlanner-Backend/Program.cs
@@ -0,0 +1,37 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+
+    app.UseDeveloperExceptionPage();
+
+    app.UseDeveloperExceptionPage();
+
+    // Do not block requests while in development
+    app.UseCors(options =>
+    {
+        options.AllowAnyOrigin();
+        options.AllowAnyMethod();
+        options.AllowAnyHeader();
+    });
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
---
+++ PUGPlanner-Backend/WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace PUGPlanner_Backend
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => TemperatureC * 3;
+
+        public string? Summary { get; set; }
+    }
+}
