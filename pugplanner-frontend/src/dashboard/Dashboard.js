413b37eac0004b7ac13822f9e3b5da91d70744b8
shanedbutler <shanedbutler@gmail.com>
2022-12-07 16:43:58 -0500

    Set game array to state

--- pugplanner-frontend/src/dashboard/Dashboard.js
+++ pugplanner-frontend/src/dashboard/Dashboard.js
@@ -1,9 +1,19 @@
+import { useEffect, useState } from "react";
+import { fetchGames } from "../managers/GameManager";
+
 export const Dashboard = () => {
-    
-    
+
+    const [games, setGames] = useState([]);
+
+    const getGames = () => fetchGames.then(games => setGames(games));
+
+    useEffect(() => {
+        getGames();
+        console.log(games);
+    }, []);
     return (
-    <>
-        Dashboard content should include cards for up-coming events that the user is on the roster / waitlist for and up-coming sign-up events
-    </>
+        <>
+            Dashboard content should include cards for up-coming events that the user is on the roster / wait-list for and up-coming sign-up events
+        </>
     )
 }
