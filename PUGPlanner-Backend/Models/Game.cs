40e00d81db57c46d1f138f8d30ee38843659c580
shanedbutler <shanedbutler@gmail.com>
2022-12-07 16:42:50 -0500

    Add game model

---
+++ PUGPlanner-Backend/Models/Game.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PUGPlanner_Backend.Models
+{
+    public class Game
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Title { get; set; }
+
+        [Required]
+        [MaxLength(255)]
+        public string Address { get; set; }
+
+        [Required]
+        public DateTime GameDate { get; set; }
+
+        [Required]
+        public DateTime SignupDate { get; set; }
+
+        [Required]
+        public int PlayerCount { get; set; }
+    }
+}
