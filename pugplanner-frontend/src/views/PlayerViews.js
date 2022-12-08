637c13f6977396099c815d7aac4edc95e938aed5
shanedbutler <shanedbutler@gmail.com>
2022-12-08 14:13:11 -0500

    Wrap views in routes

--- pugplanner-frontend/src/views/PlayerViews.js
+++ pugplanner-frontend/src/views/PlayerViews.js
@@ -1,12 +1,13 @@
-import { Route } from "react-router-dom"
+import { Route, Routes } from "react-router-dom"
 import { Dashboard } from "../dashboard/Dashboard"
-import { AppNav } from "../nav/AppNav"
 
 export const PlayerViews = () => {
 
     //<Route path="/signup/:id" element={<SignUp />} />
 
     return (
-        <Route path="/" element={<Dashboard />} />
+        <Routes>
+            <Route path="/" element={<Dashboard />} />
+        </Routes>
     )
 }
