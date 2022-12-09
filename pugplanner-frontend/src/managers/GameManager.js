c950f48df19c50e9f6b1d4f0d1c5675fa4a9adac
shanedbutler <shanedbutler@gmail.com>
2022-12-09 16:12:07 -0500

    Add fetch call for single game to manager

--- pugplanner-frontend/src/managers/GameManager.js
+++ pugplanner-frontend/src/managers/GameManager.js
@@ -1,7 +1,22 @@
 const apiUrl = "https://localhost:7066"
 
+/**
+ * Fetches all games from API
+ * @returns An array of game objects
+ */
 export const fetchGames = async () => {
     const response = await fetch(`${apiUrl}/api/game/get`);
     const games = await response.json();
     return games;
-}
+};
+
+/**
+ * Fetches single game from API by game id pk
+ * @param {int} id 
+ * @returns A game object
+ */
+export const fetchGame = async (id) => {
+    const response = await fetch(`${apiUrl}/api/game/get/${id}`);
+    const game = await response.json();
+    return game;
+};
