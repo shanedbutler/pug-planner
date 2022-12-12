842da14be80f7d10ea2d76670f64f6589659e657
shanedbutler <shanedbutler@gmail.com>
2022-12-11 19:39:12 -0500

    Refactor to fetch roster and iterate RosterItem

--- pugplanner-frontend/src/game/GameDetails.js
+++ pugplanner-frontend/src/game/GameDetails.js
@@ -1,16 +1,21 @@
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { fetchGame } from '../managers/GameManager';
+import { fetchRoster } from '../managers/UserManager';
 import { RosterItem } from './RosterItem';
 
 export const GameDetails = () => {
 
     const { id } = useParams();
     const [game, setGame] = useState({});
+    const [roster, setRoster] = useState({});
+
+    // TODO: Attach admin user to game object
 
     useEffect(() => {
-        fetchGame(id).then(game => setGame(game));        
-    }, []); 
+        fetchGame(id).then(game => setGame(game));
+        fetchRoster(id).then(roster => setRoster(roster));
+    }, []);
 
     return (
         <div className="px-5 pt-6 last:pb-6">
@@ -40,13 +45,13 @@ export const GameDetails = () => {
                             </div>
                             <div className="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
                                 <dt className="text-sm font-medium text-gray-500">Player slots</dt>
-                                <dd className="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">10 / {game.maxPlayers}</dd>
+                                <dd className="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">{roster.length} / {game.maxPlayers}</dd>
                             </div>
                             <div className="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
                                 <dt className="text-sm font-medium text-gray-500">Detailed roster</dt>
                                 <dd className="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">
                                     <ul role="list" className="divide-y divide-gray-200 rounded-md border border-gray-200">
-                                        <RosterItem />
+                                        {roster.map(player => <RosterItem key={player.id} player={player} />)}
                                     </ul>
                                 </dd>
                             </div>
--- pugplanner-frontend/src/game/RosterItem.js
+++ pugplanner-frontend/src/game/RosterItem.js
@@ -1,21 +1,19 @@
-import { PaperClipIcon } from '@heroicons/react/20/solid'
-import { CurrentUserAvatar } from '../managers/UserManager'
+import { UserAvatar } from '../managers/UserManager'
 
-export const RosterItem = ({ player }) => {
-
+export const RosterItem = ({ key, player }) => {
 
     return (
-        <li className="flex items-center justify-between py-3 pl-3 pr-4 text-sm">
+        <li className={`${(key % 2) ? "bg-gray-50" : "bg-white"} flex items-center justify-between py-3 pl-3 pr-4 text-sm`}>
             <div className="flex w-0 flex-1 items-center">
-                <CurrentUserAvatar className="h-5 w-5 flex-shrink-0 text-gray-400" aria-hidden="true" />
+                <UserAvatar fullName={player.fullName} className="h-5 w-5 flex-shrink-0 text-gray-400" aria-hidden="true" />
             </div>
             <div className="flex w-0 flex-1 items-center">
                 <div className="h-5 w-5 flex-shrink-0 text-slate-700" aria-hidden="true">
-                    Shane Butler
+                    {player.fullName}
                 </div>
             </div>
                 <a href="#" className="font-medium text-indigo-600 hover:text-indigo-500">
-                    Shane Butler
+                    {player.fullName}
                 </a>
             <div className="ml-4">
                 <span className="ml-2 w-0">Central Midfielder</span>
