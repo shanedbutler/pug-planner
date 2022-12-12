196c701a9649c0a22cfea8e16e090c2c363eb4bd
shanedbutler <shanedbutler@gmail.com>
2022-12-12 10:49:53 -0500

    Fix state error and properly pass index to child

--- pugplanner-frontend/src/game/GameDetails.js
+++ pugplanner-frontend/src/game/GameDetails.js
@@ -8,7 +8,7 @@ export const GameDetails = () => {
 
     const { id } = useParams();
     const [game, setGame] = useState({});
-    const [roster, setRoster] = useState({});
+    const [roster, setRoster] = useState([]);
 
     // TODO: Attach admin user to game object
 
@@ -33,7 +33,7 @@ export const GameDetails = () => {
                             </div>
                             <div className="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
                                 <dt className="text-sm font-medium text-gray-500">Host</dt>
-                                <dd className="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">{game.adminUser.fullName}</dd>
+                                <dd className="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">{game.adminUser?.fullName}</dd>
                             </div>
                             <div className="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
                                 <dt className="text-sm font-medium text-gray-500">Host contact</dt>
@@ -51,7 +51,7 @@ export const GameDetails = () => {
                                 <dt className="text-sm font-medium text-gray-500">Detailed roster</dt>
                                 <dd className="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">
                                     <ul className="divide-y divide-gray-200 rounded-md border border-gray-200">
-                                        {roster.map(player => <RosterItem key={player.id} player={player} />)}
+                                        {roster.map((player, i) => <RosterItem key={player.id} player={player} i={i} />)}
                                     </ul>
                                 </dd>
                             </div>
--- pugplanner-frontend/src/game/RosterItem.js
+++ pugplanner-frontend/src/game/RosterItem.js
@@ -1,9 +1,9 @@
 import { UserAvatar } from '../managers/UserManager'
 
-export const RosterItem = ({ key, player }) => {
+export const RosterItem = ({ player, i }) => {
 
     return (
-        <li className={`${(key % 2) ? "bg-gray-50" : "bg-white"} flex items-center justify-between py-3 pl-3 pr-4 text-sm`}>
+        <li className={`${(i % 2) ? "bg-gray-50" : "bg-white"} flex items-center justify-between py-3 pl-3 pr-4 text-sm`}>
             <div className="flex w-0 flex-1 items-center">
                 <UserAvatar fullName={player.fullName} className="h-5 w-5 flex-shrink-0 text-gray-400" aria-hidden="true" />
             </div>
