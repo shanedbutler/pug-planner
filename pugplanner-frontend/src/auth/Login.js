a2ea56ababed66142059224684a0dff6a0490370
shanedbutler <shanedbutler@gmail.com>
2022-12-14 15:33:41 -0500

    Remove un-used imports

--- pugplanner-frontend/src/auth/Login.js
+++ pugplanner-frontend/src/auth/Login.js
@@ -1,7 +1,6 @@
 import { useRef } from "react"
 import { Link, useNavigate } from "react-router-dom";
 import { login } from "../managers/UserManager";
-import { AuthNav } from "../nav/AuthNav";
 
 export const Login = () => {
 
