60bd18b26fc2fea9b7f5efdad40f614d64c1408e
shanedbutler <shanedbutler@gmail.com>
2022-12-14 12:17:10 -0500

    Add new button and refactor GameCard div wrappers

--- pugplanner-frontend/src/game/GameCard.js
+++ pugplanner-frontend/src/game/GameCard.js
@@ -40,63 +40,59 @@ export const GameCard = ({ game }) => {
 
     return (
         <>
-            <div className="px-5 pt-6 last:pb-6">
-                <div className="mx-auto max-w-lg sm:px-6 lg:px-8">
-                    <div className="overflow-hidden bg-white shadow rounded-md">
-                        <div className="px-4 py-5 sm:px-6">
-                            <h3 className="text-lg font-medium leading-6 text-gray-900">{game.title}</h3>
-                            <p className="mt-1 max-w-lg text-sm text-gray-500">{game.gameDateString}</p>
+            <div className="my-5 overflow-hidden bg-white shadow rounded-md">
+                <div className="px-4 py-5 sm:px-6">
+                    <h3 className="text-lg font-medium leading-6 text-gray-900">{game.title}</h3>
+                    <p className="mt-1 max-w-lg text-sm text-gray-500">{game.gameDateString}</p>
+                </div>
+                <div className="border-t border-gray-200">
+                    <dl>
+                        <div className="bg-gray-50 px-4 pt-4 pb-3 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                            <dt className="text-sm font-medium text-gray-500">Location</dt>
+                            <dd className="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">{game.location}</dd>
                         </div>
-                        <div className="border-t border-gray-200">
-                            <dl>
-                                <div className="bg-gray-50 px-4 pt-4 pb-3 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-                                    <dt className="text-sm font-medium text-gray-500">Location</dt>
-                                    <dd className="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">{game.location}</dd>
-                                </div>
-                                <div className="bg-white px-4 py-3 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-                                    <dt className="text-sm font-medium text-gray-500">Host</dt>
-                                    <dd className="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">{game.adminUser?.fullName}</dd>
-                                </div>
-                                <div className="bg-gray-50 px-4 py-3 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-                                    <dt className="text-sm font-medium text-gray-500">Roster</dt>
-                                    <dd className="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0"> {
-                                        isWaitList ?
-                                            `${game.maxPlayers} / ${game.maxPlayers} + (${rosterCount.currentPlayers} on waitlist)`
-                                            :
-                                            `${rosterCount.currentPlayers} / ${game.maxPlayers}`
-                                    }
-                                    </dd>
-                                </div>
-                                <div className="bg-white px-4 pt-3 pb-4 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-                                    <dt className="text-sm font-medium text-gray-500">About</dt>
-                                    <dd className="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">
-                                        {game.description}
-                                    </dd>
-                                </div>
-                                <div className="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-                                    <dt className="text-sm font-medium text-gray-500">Actions</dt>
-                                    <dd className="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">
-                                        <ul>
-                                            <li className="flex items-center justify-end py-2 pl-2 pr-3 text-sm">
-                                                <button
-                                                    className="rounded-md border border-transparent bg-lime-100 py-2 px-4 mr-3 text-sm font-medium text-black shadow-sm hover:bg-lime-200 focus:bg-lime-200"
-                                                    onClick={handleDetails}
-                                                >
-                                                    Details
-                                                </button>
-                                                <button
-                                                    className="rounded-md border border-transparent bg-rose-100 py-2 px-4 text-sm font-medium text-black shadow-sm hover:bg-rose-200 focus:bg-rose-200"
-                                                    onClick={handleRegister}
-                                                >
-                                                    Register
-                                                </button>
-                                            </li>
-                                        </ul>
-                                    </dd>
-                                </div>
-                            </dl>
+                        <div className="bg-white px-4 py-3 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                            <dt className="text-sm font-medium text-gray-500">Host</dt>
+                            <dd className="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">{game.adminUser?.fullName}</dd>
                         </div>
-                    </div>
+                        <div className="bg-gray-50 px-4 py-3 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                            <dt className="text-sm font-medium text-gray-500">Roster</dt>
+                            <dd className="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0"> {
+                                isWaitList ?
+                                    `${game.maxPlayers} / ${game.maxPlayers} + (${rosterCount.currentPlayers} on waitlist)`
+                                    :
+                                    `${rosterCount.currentPlayers} / ${game.maxPlayers}`
+                            }
+                            </dd>
+                        </div>
+                        <div className="bg-white px-4 pt-3 pb-4 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                            <dt className="text-sm font-medium text-gray-500">About</dt>
+                            <dd className="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">
+                                {game.description}
+                            </dd>
+                        </div>
+                        <div className="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                            <dt className="text-sm font-medium text-gray-500">Actions</dt>
+                            <dd className="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">
+                                <ul>
+                                    <li className="flex items-center justify-end py-2 pl-2 pr-3 text-sm">
+                                        <button
+                                            className="rounded-md border border-transparent bg-lime-100 py-2 px-4 mr-3 text-sm font-medium text-black shadow-sm hover:bg-lime-200 focus:bg-lime-200"
+                                            onClick={handleDetails}
+                                        >
+                                            Details
+                                        </button>
+                                        <button
+                                            className="rounded-md border border-transparent bg-rose-100 py-2 px-4 text-sm font-medium text-black shadow-sm hover:bg-rose-200 focus:bg-rose-200"
+                                            onClick={handleRegister}
+                                        >
+                                            Register
+                                        </button>
+                                    </li>
+                                </ul>
+                            </dd>
+                        </div>
+                    </dl>
                 </div>
             </div>
             <RegistrationModal open={modalOpen} setOpen={setModalOpen} handleNav={handleDetails} onDetails={false} />
