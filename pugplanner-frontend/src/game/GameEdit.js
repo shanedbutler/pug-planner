1013d4ea4a1cf0bbbb45323995a8fd4976b0c467
shanedbutler <shanedbutler@gmail.com>
2022-12-15 13:01:15 -0500

    Remove whitespace

--- pugplanner-frontend/src/game/GameEdit.js
+++ pugplanner-frontend/src/game/GameEdit.js
@@ -178,7 +178,7 @@ export const GameEdit = () => {
                                                     id="signup-time"
                                                     name="signup-time"
                                                     required
-                                                    value={game.signupTimeFormString    }
+                                                    value={game.signupTimeFormString}
                                                     ref={signupTimeRef}
                                                     className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-violet-500 focus:ring-violet-500 sm:text-sm"
                                                 />
