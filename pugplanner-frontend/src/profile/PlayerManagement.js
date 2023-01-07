a93f83c133d881426322ed87fec652af8879fcba
shanedbutler <shanedbutler@gmail.com>
2023-01-07 15:24:52 -0500

    Adjust styles for consistency

--- pugplanner-frontend/src/profile/PlayerManagement.js
+++ pugplanner-frontend/src/profile/PlayerManagement.js
@@ -15,18 +15,16 @@ export const PlayerManagement = () => {
             <div className="mt-10 sm:mt-0">
                <div className="md:col-span-1">
                   <div className="px-4 sm:px-0 text-center">
-                     <h3 className="text-3xl font-medium leading-6 text-gray-900">
-                        Player Management
-                     </h3>
-                     <p className="mt-3 text-sm text-gray-600">
-                        Disable or enable player accounts
-                     </p>
+                     <h3 className="text-3xl font-medium leading-6 text-gray-900">Player Management</h3>
+                     <p className="mt-3 text-sm text-gray-600">Disable or enable player accounts</p>
                   </div>
                </div>
                <div className="mt-5 overflow-hidden bg-white shadow rounded-md">
-                  {players.map((player, i) => (
-                     <PlayerListItem player={player} i={i} key={player.id} />
-                  ))}
+                  <ul>
+                     {players.map((player, i) => (
+                        <PlayerListItem player={player} i={i} key={player.id} />
+                     ))}
+                  </ul>
                </div>
             </div>
          </div>
