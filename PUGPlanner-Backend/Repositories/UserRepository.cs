bf09594a52d9356026a912d1b66b56099d8d9a8a
shanedbutler <shanedbutler@gmail.com>
2023-01-24 14:55:19 -0500

    Add nullable method calls to user intilization

--- PUGPlanner-Backend/Repositories/UserRepository.cs
+++ PUGPlanner-Backend/Repositories/UserRepository.cs
@@ -192,10 +192,10 @@ namespace PUGPlanner_Backend.Repositories
                 LastName = DbUtils.GetString(reader, "LastName"),
                 Email = DbUtils.GetString(reader, "Email"),
                 Phone = DbUtils.GetString(reader, "Phone"),
-                Club = DbUtils.GetString(reader, "Club"),
+                Club = DbUtils.GetNullableString(reader, "Club"),
                 PrimaryPositionId = DbUtils.GetInt(reader, "PrimaryPositionId"),
                 SecondaryPositionId = DbUtils.GetInt(reader, "SecondaryPositionId"),
-                PronounId = DbUtils.GetInt(reader, "PronounId"),
+                PronounId = DbUtils.GetNullableInt(reader, "PronounId"),
                 CreateDateTime = DbUtils.GetDateTime(reader, "CreateDateTime"),
                 Admin = DbUtils.GetBool(reader, "Admin"),
                 EmergencyName = DbUtils.GetString(reader, "EmergencyName"),
@@ -203,15 +203,15 @@ namespace PUGPlanner_Backend.Repositories
                 Active = DbUtils.GetBool(reader, "Active"),
                 Position = new UserPosition()
                 {
-                    Primary = DbUtils.GetString(reader, "PrimaryPositionName"),
-                    Secondary = DbUtils.GetString(reader, "SecondaryPositionName"),
-                    PrimaryFull = DbUtils.GetString(reader, "PrimaryPositionFullName"),
-                    SecondaryFull = DbUtils.GetString(reader, "SecondaryPositionFullName"),
+                    Primary = DbUtils.GetNullableString(reader, "PrimaryPositionName"),
+                    Secondary = DbUtils.GetNullableString(reader, "SecondaryPositionName"),
+                    PrimaryFull = DbUtils.GetNullableString(reader, "PrimaryPositionFullName"),
+                    SecondaryFull = DbUtils.GetNullableString(reader, "SecondaryPositionFullName"),
                 },
                 Pronoun = new Pronoun()
                 {
-                    Id = DbUtils.GetInt(reader, "PronounId"),
-                    Name = DbUtils.GetString(reader, "PronounName"),
+                    Id = DbUtils.GetNullableInt(reader, "PronounId"),
+                    Name = DbUtils.GetNullableString(reader, "PronounName"),
                 }
             };
         }
