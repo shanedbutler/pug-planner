e0c75b13985cdfc064ba3cf365b2961721035e2e
shanedbutler <shanedbutler@gmail.com>
2023-02-17 13:38:39 -0500

    Refactor with SPA Proxy library

--- PUGPlanner-Backend/Program.cs
+++ PUGPlanner-Backend/Program.cs
@@ -20,6 +20,12 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -37,9 +43,15 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseRouting();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller}/{action=Index}/{id?}");
+
+app.MapFallbackToFile("index.html"); ;
 
 app.UseAuthorization();
-
-app.MapControllers();
 
 app.Run();
