61dd01933ab337443ce4ee5e12ca963225a0eb36
shanedbutler <shanedbutler@gmail.com>
2023-02-18 13:55:12 -0500

    Add Firebase Auth and configure

--- Program.cs
+++ Program.cs
@@ -1,3 +1,8 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using PUGPlanner_FS.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -9,13 +14,58 @@ builder.Services.AddTransient<IPronounRepository, PronounRepository>();
 builder.Services.AddTransient<IGameRepository, GameRepository>();
 builder.Services.AddTransient<IRosterRepository, RosterRepository>();
 
+var config = new ConfigurationBuilder()
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("appsettings.json")
+    .Build();
+
+string firebaseProjectId = ConfigurationBinder.GetValue<string>(config, "FirebaseProjectId");
+string googleTokenUrl = $"https://securetoken.google.com/{firebaseProjectId}";
+builder.Services
+    .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.Authority = googleTokenUrl;
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidIssuer = googleTokenUrl,
+            ValidateAudience = true,
+            ValidAudience = firebaseProjectId,
+            ValidateLifetime = true
+        };
+    });
+
 builder.Services.AddControllers();
 
 //builder.options.UseSqlServer(Configuration.GetConnectionString("defaultConnection")));
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "PUGPlanner", Version = "v1" });
+
+    var securitySchema = new OpenApiSecurityScheme
+    {
+        Name = "Authorization",
+        BearerFormat = "JWT",
+        Description = "JWT Authorization header using the Bearer scheme.",
+        Type = SecuritySchemeType.ApiKey,
+        In = ParameterLocation.Header,
+        Reference = new OpenApiReference
+        {
+            Id = "Bearer",
+            Type = ReferenceType.SecurityScheme,
+        }
+    };
+
+    c.AddSecurityDefinition("Bearer", securitySchema);
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        { securitySchema, new[] { "Bearer"} }
+    });
+});
 
 var app = builder.Build();
 
@@ -52,6 +102,7 @@ app.MapControllerRoute(
 
 app.MapFallbackToFile("index.html"); ;
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.Run();
