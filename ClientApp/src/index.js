a5b330b6f9bf8e22d599aef702ec1f2e6d7a6cb3
shanedbutler <shanedbutler@gmail.com>
2023-02-22 13:53:16 -0500

    Add initialization of firebase to front-end client

--- ClientApp/src/index.js
+++ ClientApp/src/index.js
@@ -5,6 +5,12 @@ import './input.css';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 import { App } from './App';
+import firebase from "firebase/app";
+
+const firebaseConfig = {
+   apiKey: process.env.REACT_APP_API_KEY,
+ };
+ firebase.initializeApp(firebaseConfig);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
