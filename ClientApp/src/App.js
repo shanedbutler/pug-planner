d1110833aff93c2212c532a521983b6bd0ffc44e
shanedbutler <shanedbutler@gmail.com>
2023-03-08 17:20:52 -0500

    Add hooks to app for getting logged in user

--- ClientApp/src/App.js
+++ ClientApp/src/App.js
@@ -4,8 +4,33 @@ import { Register } from './auth/Register';
 import { AppNav } from './nav/AppNav';
 import { ApplicationViews } from './views/ApplicationViews';
 import { Authorized } from './views/Authorized';
+import { onLoginStatusChange, me } from './managers/AuthManager';
+import { useEffect, useState } from 'react';
 
 export const App = () => {
+   const [isLoggedIn, setIsLoggedIn] = useState(null);
+   const [userProfile, setUserProfile] = useState(null);
+ 
+   useEffect(() => {
+     onLoginStatusChange(setIsLoggedIn);
+   }, []);
+ 
+   useEffect(() => {
+     if (isLoggedIn) {
+       me().then(setUserProfile);
+     } else {
+       setUserProfile(null);
+     }
+   }, [isLoggedIn]);
+ 
+   // The "isLoggedIn" state variable will be null until //  the app's connection to firebase has been established.
+   //  Then it will be set to true or false by the "onLoginStatusChange" function
+   if (isLoggedIn === null) {
+     // Until we know whether or not the user is logged in or not, just show a spinner
+     // NOTE: To-do, add spinner
+     return null;
+   }
+
    return (
       <Routes>
          <Route path="/login" element={<Login />} />
@@ -14,7 +39,7 @@ export const App = () => {
          <Route
             path="*"
             element={
-               <Authorized>
+               <Authorized isLoggedIn={isLoggedIn} userProfile={userProfile} >
                   <>
                      <AppNav />
                      <div className="content-wrapper selection:bg-lime-100">
