ef318217e3efccd1f2d04f0c1980e35003c65547
shanedbutler <shanedbutler@gmail.com>
2023-03-09 17:16:58 -0500

    Refactor user state w localStorage and useContext

--- ClientApp/src/App.js
+++ ClientApp/src/App.js
@@ -5,50 +5,64 @@ import { AppNav } from './nav/AppNav';
 import { ApplicationViews } from './views/ApplicationViews';
 import { Authorized } from './views/Authorized';
 import { onLoginStatusChange, me } from './managers/AuthManager';
-import { useEffect, useState } from 'react';
+import { createContext, useEffect, useState } from 'react';
+
+const UserContext = createContext(null);
 
 export const App = () => {
    const [isLoggedIn, setIsLoggedIn] = useState(null);
    const [userProfile, setUserProfile] = useState(null);
- 
+
    useEffect(() => {
-     onLoginStatusChange(setIsLoggedIn);
+      onLoginStatusChange(setIsLoggedIn);
    }, []);
- 
+
    useEffect(() => {
-     if (isLoggedIn) {
-       me().then(setUserProfile);
-     } else {
-       setUserProfile(null);
-     }
+      if (isLoggedIn) {
+         me().then(setUserProfile);
+         localStorage.setItem(
+            'userProfile',
+            JSON.stringify({
+               id: userProfile.id,
+               fullName: userProfile.fullName,
+               email: userProfile.email,
+               admin: userProfile.admin,
+            })
+         );
+      } else {
+         setUserProfile(null);
+         localStorage.removeItem("userProfile");
+      }
    }, [isLoggedIn]);
- 
+
    // The "isLoggedIn" state variable will be null until //  the app's connection to firebase has been established.
    //  Then it will be set to true or false by the "onLoginStatusChange" function
    if (isLoggedIn === null) {
-     // Until we know whether or not the user is logged in or not, just show a spinner
-     // NOTE: To-do, add spinner
-     return null;
+      // Until we know whether or not the user is logged in or not, we could show a spinner
+      // NOTE: To-do, add spinner
+      return null;
    }
 
    return (
-      <Routes>
-         <Route path="/login" element={<Login />} />
-         <Route path="/register" element={<Register />} />
+      <UserContext.Provider value={(userProfile, isLoggedIn)}>
+         <Routes>
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
 
-         <Route
-            path="*"
-            element={
-               <Authorized isLoggedIn={isLoggedIn} userProfile={userProfile} >
-                  <>
-                     <AppNav />
-                     <div className="content-wrapper selection:bg-lime-100">
-                        <ApplicationViews />
-                     </div>
-                  </>
-               </Authorized>
-            }
-         />
-      </Routes>
+            <Route
+               path="*"
+               element={
+                  <Authorized isLoggedIn={isLoggedIn}>
+                     <>
+                        <AppNav />
+                        <div className="content-wrapper selection:bg-lime-100">
+                           <ApplicationViews />
+                        </div>
+                     </>
+                  </Authorized>
+               }
+            />
+         </Routes>
+      </UserContext.Provider>
    );
 };
