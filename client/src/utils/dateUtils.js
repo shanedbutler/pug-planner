5723344fe9409c816ddb7082b246a2788eede877
Shane Butler <shanedbutler@gmail.com>
2023-11-05 10:18:42 -0500

    Add date util fn checks if iso date is passed

--- client/src/utils/dateUtils.js
+++ client/src/utils/dateUtils.js
@@ -1,6 +1,7 @@
 /**
  * Convert a ISO date string to a human readable date string
  * @param {string} isoDate - ISO date string
+ * @returns string
  */
 export const formatIsoDate = (isoDate) => {
     const date = new Date(isoDate);
@@ -13,3 +14,14 @@ export const formatIsoDate = (isoDate) => {
     };
     return date.toLocaleDateString('en-US', options);
 }
+
+/**
+ * Check if a ISO date string is in the past
+ * @param {string} isoDate - ISO date string
+ * @returns boolean
+ */
+export const isPastDate = (isoDate) => {
+    const date = new Date(isoDate);
+    const currentDate = new Date();
+    return date < currentDate;
+};
